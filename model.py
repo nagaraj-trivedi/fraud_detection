ca1b0806498933b9e24adeddb16ebde10fd05c17
nagaraj-trivedi <trivedi.nagaraj@gmail.com>
2021-11-27 12:39:33 +0530

    Added new model code

--- model.py
+++ model.py
@@ -1,1 +1,11 @@
-print("Adding all ML related code here')
+print("Adding all ML related code here")
+
+def model1():
+    print("Adding model1")
+
+def model2():
+    print("Adding model2")
+
+def model3():
+    print("Adding model3")
+
